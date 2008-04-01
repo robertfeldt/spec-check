805eb28d066533aac1344f8499f601d9a420b734
Robert Feldt <robert.feldt@gmail.com>
2008-04-01 15:39:46 +0200

    updated latest_stable copy since many bugs have now been fixed

--- latest_stable/spec-check.clj
+++ latest_stable/spec-check.clj
@@ -1,5 +1,5 @@
 ;; spec-check - 
-;;   specification and autochecking of software behavior expectations
+;;   Specification & checking of software behavior expectations
 ;;
 ;; Copyright (c) Robert Feldt. All rights reserved.
 ;; The use and distribution terms for this software are covered by the
@@ -24,15 +24,19 @@
 	    ""
 	    (str start (reduce (fn [acc e] (str acc separator (print-str e))) values)))))
 
-(defn- trial-outcome-description [outcome]
+(defn- exception? [outcome] (= (class outcome) (class {})))
+(defn- failure-or-exception? [outcome] (or (not outcome) (exception? outcome)))
+
+(defn trial-outcome-description [outcome]
   (cond
-    (= true  outcome) \.
-	(= false outcome) \F
-	:else             \E))
+    (= true  outcome)    \.
+	(= false outcome)    \F
+	(exception? outcome) \E
+	:else                \U)) ; U just for debugging purposes
 
 (defn- print-progress [outcome]
 	(print (trial-outcome-description outcome))
-	(. *out* (flush)))	
+	(. *out* (flush)))
 
 (def *failing-trials*)
 (def *checking-cases* false)
@@ -44,27 +48,30 @@
 	not= (fn [f as] (str "arguments *ARE* the same: " (join as)))
 })
 
-(defn- exception? [outcome] (and (= (class outcome) (class {})) (= (:outcome outcome) 'exception)))
-(defn- failure-or-exception? [outcome] (or (not outcome) (exception? outcome)))
+(defn- trial-problem-message [type descstack & messagestrings]
+  (if (> (count descstack) 0)
+    (str type " in " (join descstack " | ") (join messagestrings ""))
+    (str type (join messagestrings ""))))
 
-(defn- show-failing-trial [code outcome fnfn argsfn]
-	(if (exception? outcome)
-	  (str "EXCEPTION:\n  the code was: " code ", but\n  it raised " (:exception outcome))
-	  (let [func (fnfn) 
-		    report-fn (or (get *inverted-reporting-fns* func) (fn [f as] (str "arguments was: " (print-str (first as)))))]
-		 (str "FAILURE:\n  the code was: " code ", but\n  " (report-fn func (argsfn))))))
+(defn show-failing-trial [code outcome fnfn argsfn descstack]
+  (cond
+    (exception? outcome)
+	  (trial-problem-message "EXCEPTION" descstack 
+		"\n  expectation was: " code ", but\n  it raised " (:exception outcome))
+	(= false outcome)
+      (let [func (fnfn) 
+		    report-fn (or (get *inverted-reporting-fns* func) (fn [f as] (str "arguments was: " (pr-str (first as)))))]
+		 (trial-problem-message "FAILURE" descstack 
+			"\n  expectation was: " code ", but\n  " (report-fn func (argsfn))))))
 
-(defn- log-trial [code outcome fnfn argsfn]
+(defn- log-trial [code outcome fnfn argsfn descstack]
 	(set! *num-trials* (+ 1 *num-trials*))
 	(if (failure-or-exception? outcome)
-		(set! *failing-trials* (conj *failing-trials* (show-failing-trial code outcome fnfn argsfn)))))
+		(set! *failing-trials* (conj *failing-trials* (show-failing-trial code outcome fnfn argsfn descstack)))))
 
 (defn- report-all-trials [seconds-taken]
-  (newline)
-  (println (join *failing-trials* "\n"))
-  (newline)
-  (println "Finished in" seconds-taken "seconds")
-  (newline)
+  (println (join *failing-trials* "\n" "\n")) (newline)
+  (println "Finished in" seconds-taken "seconds") (newline)
   (println *num-trials* "expectations checked," (count *failing-trials*) "failed"))
 
 (def *report-progress* print-progress)
@@ -72,15 +79,16 @@
 (def *report-all-trials* report-all-trials)
 (def *spec-stack*)
 
-(defmacro ismacro [code & fn-and-args]
+(defmacro expectation [code & fn-and-args]
+  "An expectation that FN applied to ARGS should return true that uses
+   CODE to document what was checked."
   `(let [outcome# (try (~@fn-and-args) (catch java.lang.Exception e# {:outcome 'exception :exception e#}))]
 	(*report-progress* outcome#)
-	(*log-trial* ~code outcome# (fn [] ~(first fn-and-args)) (fn [] [~@(rest fn-and-args)]))))
+	(*log-trial* ~code outcome# (fn [] ~(first fn-and-args)) (fn [] [~@(rest fn-and-args)])
+	             *spec-stack*)))
 
-(defmacro is [& fn-and-args]
-  "An expectation that FN applied to ARGS should return true."
-  `(ismacro (str "(is" ~@(for [e# fn-and-args] (str " " (print-str e#))) ")") 
-	        ~(first fn-and-args) ~@(rest fn-and-args)))
+(defmacro codestr [exp & parts]
+  `(str "(" ~exp ~@(for [p# parts] (str " " (print-str p#))) ")"))
 
 (defmacro spec [desc & body]
   "A spec is described by DESC and defined by expectations and specs in
@@ -91,8 +99,9 @@
   "A function spec (fspec) is a spec associated with the function FUNC. 
    It is is described by DESC and defined by expectations and specs in
    BODY."
-	`(do ;(*cache-spec* ~func ~desc ~forms)
-	     ~@body))
+	`(binding [*spec-stack* (conj *spec-stack* (str (if (= 'nil '~func) "" (str '~func "/")) ~desc))]
+		;(*cache-spec* ~func ~desc ~forms)
+	     (do ~@body)))
 
 (defmacro just-time
   "Evaluates expr and returns the number of seconds it took."
@@ -113,7 +122,15 @@
 	     (*report-all-trials* timetaken#)))))
 
 ;; Utility functions and macros that builds on the core
+(defmacro is [& fn-and-args]
+  "An expectation that FN applied to ARGS should return true."
+  `(expectation (codestr "is" ~@fn-and-args) ~(first fn-and-args) ~@(rest fn-and-args)))
+
 (defmacro isnt [& fn-and-args]
-  "An expectation that FN applied to ARGS should return false."
-  `(ismacro (str "(isnt" ~@(for [e# fn-and-args] (str " " (print-str e#))) ")") 
-	        (complement ~(first fn-and-args)) ~@(rest fn-and-args)))
+  "An expectation that FN applied to ARGS should return true."
+  `(expectation (codestr "isnt" ~@fn-and-args) (complement ~(first fn-and-args)) ~@(rest fn-and-args)))
+
+(defmacro all-are [checkfn & forms]
+  "State multiple expectations in one go, namely that 
+   CHECKFN applied to each form in FORMS should return true."
+  `(for [f# [~@forms]] `(is ~~checkfn ~f#)))
